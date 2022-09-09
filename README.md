69334e869c758207d17e0bfc114d6567ad899f0e
Ronan Singpurwala <singpurwala13@gmail.com>
2022-09-10 00:30:50 +0100

    a

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # List-Application
+
+## Description
+- This is list application made with PHP with a SQL database.
+- It can be used as a to-do list or anything you would put on a list
+
+## Features
+- Add to list
+- Delete from list
+- Clear list
+- Time stamp of when you added something to the list
+
+Click [here](https://shopping-list-application.000webhostapp.com/index.php) to demo.
--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="assets/scripts/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <title>shopping list</title>
+    <title>Shopping List</title>
     <link rel="icon" href="assets/img/icon.png" type="image/x-icon">
 </head>
 
